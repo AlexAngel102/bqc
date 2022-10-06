ac2438b97bfc7909ba9ded56abfdf98b32a5f52e
Aleksandr Petrik <alexangel102@gmail.com>
2022-10-06 14:43:53 +0300

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function creteUser()
+    {
+
+    }
+
+    public function getUser()
+    {
+
+    }
+
+    public function updateUser()
+    {
+
+    }
+
+    public function deleteUser()
+    {
+
+    }
+}
